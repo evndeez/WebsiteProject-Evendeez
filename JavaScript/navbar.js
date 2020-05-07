138f8313ca760fc7102c90242f35fc12a34cbfc3
evndeez <64960601+evndeez@users.noreply.github.com>
2020-05-07 23:46:27 +0500

    Add files via upload

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,7 +1,21 @@
 
   $(document).ready(function () {
+
     $('.navbar-nav .nav-item').click(function(){
       $('.navbar-nav .nav-item').removeClass('active');
       $(this).addClass('active');
-  });
+    });
+
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      if(scroll >= 200)
+      {
+        $('#navigationBar').css({"background-color": "black", "box-shadow": "0 5px 5px rgb(68, 68, 68)"})
+      }
+      else
+      {
+        $('#navigationBar').css({"background-color": "rgba(0,0,0,0)", "box-shadow": "0 0 0 0"})
+      }
+    });
+
 });
